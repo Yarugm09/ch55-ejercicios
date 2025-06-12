73314a9b1c05fdb80a76910e4df3093c1dfdc40c
Yarugm <yarumi.gmdev@gmail.com>
2025-06-12 16:36:24 -0600

    Finish excercise histernumeber

---
+++ logica/histernumber.js
@@ -0,0 +1,11 @@
+/*1.preguntar 10 numeros  al usuario 
+2.Elegir el numero mas alto de esos 10 numeros 
+3.Mostrar el numero mas alto en consola.
+*/
+const numeros = [3,59,384,24,45,6,73,2];  
+let max = Math.max(... numeros);
+console.log(max);
+
+
+
+
